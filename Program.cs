56d1f5353b79b6f0f0d9c4cdbdcd6f1c73613f7e
Fadde <obeyfranso@gmail.com>
2023-08-29 18:42:00 +0200

    feat: method to fetch word from UD

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ using Newtonsoft.Json;
 
 public class Program
 {
+
+    private DiscordSocketClient? client;
+    private string api_path = "https://api.urbandictionary.com/v0/define?term=";
     public static Task Main(string[] args) => new Program().MainAsync();
 
     private Task Log(LogMessage msg)
@@ -12,7 +15,20 @@ public class Program
         return Task.CompletedTask;
     }
 
-    private DiscordSocketClient? client;
+    public async Task<string> FetchDefinitionFromUD(string word)
+    {
+        using HttpClient client = new();
+
+        HttpResponseMessage response = await client.GetAsync(api_path + word);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return $"Error: {response.StatusCode}";
+        }
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+        return responseBody;
+    }
     public async Task MainAsync()
     {
         client = new DiscordSocketClient();
@@ -21,6 +37,10 @@ public class Program
         var token = JsonConvert.DeserializeObject<ConfigurationClass>(File.ReadAllText("config.json"))?.Token;
         await client.LoginAsync(TokenType.Bot, token);
         await client.StartAsync();
+
+        string response = await FetchDefinitionFromUD("fahad");
+        Console.WriteLine(response);
+
 
         // Block until the program is closed.
         await Task.Delay(-1);
