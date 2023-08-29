1e01f56f54443c17ae9f559b3ef8ff6765bad1b1
Fadde <obeyfranso@gmail.com>
2023-08-29 18:28:08 +0200

    feat: Bot comes online

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+    private DiscordSocketClient? client;
+    public async Task MainAsync()
+    {
+        client = new DiscordSocketClient();
+        client.Log += Log;
+
+        var token = JsonConvert.DeserializeObject<ConfigurationClass>(File.ReadAllText("config.json"))?.Token;
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+
+        // Block until the program is closed.
+        await Task.Delay(-1);
+    }
+}
